f051e427a9da1497e1097972a16a01d4ec2ddfc5
19tanisha <tanishavanneri19@gmail.com>
2021-04-15 22:13:35 +0530

    Compression

--- Compress1/comp.py
+++ Compress1/comp.py
@@ -4,7 +4,7 @@ data = open('demo.txt', 'r').read()
 data_byte = bytes(data, 'utf-8')
 data_compress = base64.b64encode(zlib.compress(data_byte))
 decode_file = data_compress.decode('utf-8')
-open('compress.txt', 'w').write(decode_file)
+open('tanya.txt', 'w').write(decode_file)
 
 decompress_data = zlib.decompress(base64.b64decode(decode_file))
 print(decompress_data)
