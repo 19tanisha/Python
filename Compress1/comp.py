ddf3ab9bcf9e31950dd10c57bc903c4c3e51cb3f
Tanya Thakur <71583466+tanya1019@users.noreply.github.com>
2021-04-15 22:30:36 +0530

    Added function for decompression

--- Compress1/comp.py
+++ Compress1/comp.py
@@ -6,5 +6,6 @@ data_compress = base64.b64encode(zlib.compress(data_byte))
 decode_file = data_compress.decode('utf-8')
 open('tanya.txt', 'w').write(decode_file)
 
+#Decompression
 decompress_data = zlib.decompress(base64.b64decode(decode_file))
 print(decompress_data)
